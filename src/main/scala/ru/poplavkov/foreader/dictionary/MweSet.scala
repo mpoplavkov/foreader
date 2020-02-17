8c96554b3d5fd847b96c701aec480e86323875f4
mpoplavkov <mihail.poplavkov@gmail.com>
2020-02-17 16:34:45 +0300

    Change list to seq

--- src/main/scala/ru/poplavkov/foreader/dictionary/MweSet.scala
+++ src/main/scala/ru/poplavkov/foreader/dictionary/MweSet.scala
@@ -13,11 +13,11 @@ trait MweSet[F[_]] {
 
   /**
     * Returns all multi-word expressions starting with the `startWord`
-    * Each MWE represented as a list of words in order and doesn't include the `startWord`
+    * Each MWE represented as a seq of words in order and doesn't include the `startWord`
     *
     * @param startWord word to lookup MWEs starting with it
     * @return set of MWEs or an empty set if no MWEs found
     */
-  def getMwesStartingWith(startWord: Word): F[Set[List[Word]]]
+  def getMwesStartingWith(startWord: Word): F[Set[Seq[Word]]]
 
 }

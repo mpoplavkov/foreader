923762f6e463505610377daf229f782ac1099629
mpoplavkov <mihail.poplavkov@gmail.com>
2020-03-21 17:10:38 +0300

    Do not log text

--- core/src/main/scala/ru/poplavkov/foreader/text/logged/LoggedTokenExtractor.scala
+++ core/src/main/scala/ru/poplavkov/foreader/text/logged/LoggedTokenExtractor.scala
@@ -11,6 +11,6 @@ import scala.language.higherKinds
 trait LoggedTokenExtractor[F[_]] extends TokenExtractor[F] with LoggingF[F] {
 
   abstract override def extract(text: String): F[Seq[Token]] =
-    super.extract(text).loggedF("extract", Map("text" -> text))()
+    super.extract(text).loggedF("extract", Map("text" -> "possibly huge text"))()
 
 }
